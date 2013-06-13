13fa0f5343e7d8ce4bcb91647b255a96d0c43a03
Eric Goeckeritz <ergoe@me.com>
2013-06-13 13:49:24 -0700

    Added participant entry form with validation

--- Trek/Controllers/ParticipantEntryController.cs
+++ Trek/Controllers/ParticipantEntryController.cs
@@ -1,38 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 using Trek.Models;
+using System.Net;
 
 namespace Trek.Controllers
 {
     public class ParticipantEntryController : Controller
     {
-        //
-        // GET: /ParticipantEntry/
-
-        public ActionResult Index()
-        {
-            return View();
-        }
-
-        //
-        // GET: /ParticipantEntry/Details/5
-
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
-        //
-        // GET: /ParticipantEntry/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
         //
         // POST: /ParticipantEntry/Create
 
@@ -46,62 +25,66 @@ namespace Trek.Controllers
 
         public ActionResult AddParticipant(ParticipantModel participant)
         {
-            string name = participant.Fname;
-            JsonResult jsonResult = Json(participant);
-            var b = jsonResult.Data;
-            return null;
-        }
+            //var p = new ParticipantModel("12330", "Eric", "Goeckeritz", "16", "M", "12", "2013-12-22", "5910 Joe Lane",
+            //                              "address2", "2086956598", "Lara Goeckeritz", "2086088044",
+            //                              "This is the considerations field", "Blue Cross", "zqdd1234d",
+            //                              "This is the camput arrangment box");
 
-        //
-        // GET: /ParticipantEntry/Edit/5
+            string id = Guid.NewGuid().ToString().Substring(0, 8);
+            participant.UserId = id;
+            //main api
+            //string requestUrl = @"http://www.truetothefaithtrek.com/_trekapi/api/participant";
+            //localhost
+            string requestUrl = @"http://localhost:49167/api/participant";
 
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
+            var request = HttpWebRequest.Create(requestUrl);
 
-        //
-        // POST: /ParticipantEntry/Edit/5
+            request.Method = "POST";
 
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
+            request.ContentType = "application/json; charset=utf-8";
+            var jsonObj = JsonConvert.SerializeObject(participant);
+
+            var djsonOject = JsonConvert.DeserializeObject<ParticipantModel>(jsonObj);
+
             try
             {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
+                using (StreamWriter requestWritter = new StreamWriter(request.GetRequestStream()))
+                {
+                    requestWritter.Write(jsonObj);
+                }
             }
-            catch
+            catch (WebException ex)
             {
-                return View();
+                return Content("");
             }
+
+            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            {
+                if (response.StatusCode != HttpStatusCode.OK)
+                {
+                    ModelState.AddModelError("", "Request status was not OK. Please try again");
+                }
+                using (StreamReader reader = new StreamReader(response.GetResponseStream()))
+                {
+                    var serverResponse = reader.ReadToEnd();
+                }
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    //Redirect("Home/ParticipantEntry");
+                    //return View("ParticipantEntry");
+                }
+            }
+
+
+            //string name = participant.Fname;
+            //JsonResult jsonResult = Json(participant);
+            //var b = jsonResult.Data;
+            //ViewBag.Participant = Json(participant, JsonRequestBehavior.AllowGet);
+            //return Json(p, JsonRequestBehavior.AllowGet);
+            ModelState.Clear();
+            return View("ParticipantEntry");
         }
 
-        //
-        // GET: /ParticipantEntry/Delete/5
-
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /ParticipantEntry/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
+        
     }
 }
--- Trek/Models/ParticipantModel.cs
+++ Trek/Models/ParticipantModel.cs
@@ -2,19 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using DataAnnotationsExtensions;
 
 namespace Trek.Models
 {
     public class ParticipantModel
     {
         public string UserId { get; set; }
+
+        [DisplayName("First Name")]
+        [Required(ErrorMessage = "*Required")]
         public string Fname { get; set; }
+
+        [DisplayName("Last Name")]
+        [Required(ErrorMessage = "*Required")]
         public string Lname { get; set; }
-        public string FamilyId { get; set; }
-        public string Role { get; set; }
-        public string ClassName { get; set; }
-        public string BirthDate { get; set; }
+
+        [DisplayName("Ward")]
+        [Required(ErrorMessage = "*Required")]
+        [Integer(ErrorMessage = "Needs to be a number!")]
+        public string Ward { get; set; }
+
+        [DisplayName("Gender")]
+        [Required(ErrorMessage = "*Required")]
+        public string Gender { get; set; }
+
+        [DisplayName("Age")]
+        [Required(ErrorMessage = "Age is required")]
+        [Integer(ErrorMessage = "This needs to be a number!")]
+        [Range(12,99)]
         public string Age { get; set; }
 
+        public string BirthDate { get; set; }
+        public string Address1 { get; set; }
+        public string Address2 { get; set; }
+        public string Phone { get; set; }
+        public string ParentGaurdian { get; set; }
+        public string ParentPhone { get; set; }
+        public string Considerations { get; set; }
+        public string MedicalInsruance { get; set; }
+        public string PolicyNumber { get; set; }
+        public string CampoutArrangments { get; set; }
+
+        // public ParticipantModel(string userId, string fname, string lname, string ward, string gender, string age, string birthdate, string address1,
+        //    string address2, string phone, string parentGaurdian, string parentPhone, string considerations, string medicalInsurance, string policyNumber, string campoutArrangements)
+        //{
+        //    UserId = userId;
+        //    Fname = fname;
+        //    Lname = lname;
+        //    Ward = ward;
+        //    Gender = gender;
+        //    Age = age;
+        //    BirthDate = birthdate;
+        //    Address1 = address1;
+        //    Address2 = address2;
+        //    Phone = phone;
+        //    ParentGaurdian = parentGaurdian;
+        //    ParentPhone = parentPhone;
+        //    Considerations = considerations;
+        //    MedicalInsruance = medicalInsurance;
+        //    PolicyNumber = policyNumber;
+        //    CampoutArrangments = campoutArrangements;
+        //}
+        public ParticipantModel()
+        {
+            
+        }
     }
 }
